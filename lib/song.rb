b1f8d307dae65cca4ea73f35bdb861ca63fde53c
Martin Achtermann <mdacht777@gmail.com>
2020-06-28 14:14:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,15 +11,15 @@ class Song
   def self.all
     @@all
   end
-  def self.artist_name
-      # binding.pry
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
   def self.new_by_filename(file)
     file = file.chomp(".mp3").split(" - ")
     song = Song.new(file[1])
     artist_name = file[0]
     song
   end  
+  def artist_name
+      # binding.pry
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
 end
